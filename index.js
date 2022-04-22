88f5fe16fc4ea87cdb8bfd655bbc74229869565d
dwil13 <dw22197@gmail.com>
2022-04-23 01:59:31 +0530

    added features
    1. implemented fallback 2. set up image queue for optimising number of requests

--- index.js
+++ index.js
@@ -1,39 +1,127 @@
-//input parameters
-const baseUrl = `https://g.tenor.com/v1/random`;
-const apikey = `MQEGHTEYHEJE`;
-const limit = 1;
-const contentfilter = `medium`;
-const media_filter = `basic`;
-const ar_range = `wide`;
+//user input
+let query = window.sessionStorage.getItem(`q`);
 
-let query = window.localStorage.getItem(`q`);
-
-if (query == null) {
+while (query == null) {
   query = prompt(`What would you like to see?`);
-  window.localStorage.setItem(`q`, query);
-} else {
-  query = window.localStorage.getItem(`q`);
+  window.sessionStorage.setItem(`q`, query);
 }
 
-const queryF = replaceWithHyphen(query);
-async function start() {
+if (query.includes(` `)) {
+  query = replaceWithHyphen(query);
+}
+
+//query parameters
+const baseUrl = `https://g.tenor.com/v1/random`;
+const apikey = `MQEGHTEYHEJE`;
+const limit = 10;
+const contentfilter = `medium`; //(values: off | low | medium | high)
+const media_filter = `basic`;
+const ar_range = `wide`;
+const whiteSpace = document.getElementById(`container`);
+const body = document.getElementsByTagName(`body`);
+let pos = 0;
+
+//dom elements
+let hero = document.getElementById(`hero`);
+
+//Other global variables
+let buffer = limit - 1; //to be less than limit at all times
+let resultSet;
+let images = [];
+let current = 0;
+
+if (buffer >= limit) {
+  console.error(`buffer exceeds limit. adjusting...`);
+  buffer = limit - 5;
+  console.warn(`buffer set to ${buffer}`);
+}
+
+/********** Functions **********/
+main();
+
+async function main() {
+  console.log(`Entered Main`);
   try {
-    const searchUrl = `${baseUrl}?q=${queryF}&key=${apikey}$limit=${limit}&contentfilter=${contentfilter}&media_filter=${media_filter}&ar_range=${ar_range}`;
-    console.log(`Search url`, searchUrl);
-    const response = await fetch(searchUrl);
-    data = await response.json();
-    const resourceUrl = data.results[0].media[0].gif.url;
-    //update with new resource url
-    updateImg(resourceUrl);
+    await fetchData(prepareImages);
+    updatePage();
   } catch (error) {
-    console.log(error);
-    updateImg(`img/2.gif`);
+    fallback();
   }
 }
 
-function updateImg(url) {
-  let hero = document.getElementById(`hero`);
+async function fetchData(callback) {
+  console.log(`%c Fetching data `, "background:green; color:white;");
+
+  /**********Query**********/
+
+  let searchUrl = `${baseUrl}?q=${query}&key=${apikey}&limit=${limit}&contentfilter=${contentfilter}&media_filter=${media_filter}&ar_range=${ar_range}&pos=${pos}`;
+
+  try {
+    console.log(`%c${searchUrl}`, `background: black; color: blue`);
+    const response = await fetch(searchUrl);
+    resultSet = await response.json();
+    pos = resultSet.next;
+    const imageData = resultSet.results;
+    console.log(`imageData ->`, imageData);
+    console.log(`Fetch successful`);
+
+    callback(imageData);
+  } catch (error) {
+    console.error(`Fetch failed`);
+    throw error;
+  }
+}
+
+async function updatePage() {
+  try {
+    if (current === buffer || images.length == 0) {
+      await fetchData(prepareImages);
+
+      if (images.length > limit) {
+        images.splice(0, buffer + 1);
+        console.log(
+          `%c Removing first ${buffer + 1} element. ${
+            images.length
+          } image URLs available`,
+          `background: black; color: yellow`
+        );
+      }
+
+      current = 0;
+      console.log(`index reset to ->`, current);
+    }
+
+    if (images.length == 0) {
+      console.error(`No URLs in queue`);
+      return;
+    }
+
+    console.log(`updating page with index -> `, current);
+    let url = images[current++];
+    updateImage(url);
+  } catch (error) {
+    console.error(`Couldn't update image from data`);
+    fallback();
+  }
+}
+
+/**
+ * Utility Functions
+ */
+
+function prepareImages(imageData) {
+  console.log(`Preparing images`);
+
+  imageData.forEach((imageData) => {
+    images.push(imageData.media[0].gif.url);
+  });
+
+  console.log(`${images.length} image URLs ready`);
+}
+
+function updateImage(url) {
   hero.src = url;
+  console.log(`Hero image updated`);
 }
 
 function replaceWithHyphen(string) {
@@ -43,4 +131,12 @@ function replaceWithHyphen(string) {
   return newString;
 }
 
-start();
+function fallback() {
+  console.warn(`Executing fallback`);
+  updateImage(`img/2.gif`);
+}
+
+hero.addEventListener(`click`, () => {
+  console.log(`Next image request`);
+  updatePage();
+});
